f01950e82fb440c69f6050a18b66abc9a4537ca9
maksi <maksim.v1liev@gmail.com>
2026-02-08 11:48:36 +0300

    calendar icon

--- app/src/main/java/com/example/forecast/ui/descriptions/Description.kt
+++ app/src/main/java/com/example/forecast/ui/descriptions/Description.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -27,51 +26,43 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.forecast.R
 import com.example.forecast.domain.Weather
 import com.example.forecast.ui.formatterFuture
 import com.example.forecast.ui.getPainterResource
+import com.example.forecast.ui.theme.MainGradient
 import com.example.forecast.ui.theme.textStyle
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.Locale
 import kotlin.math.round
 
 @Composable
-fun ShowScreen(weathersToday: List<Weather>,
-               weathersForecast: List<Weather>,
-               onBack: () -> Unit) {
+fun ShowScreen(
+    weathersToday: List<Weather>,
+    weathersForecast: List<Weather>,
+    onBack: () -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(
-                Brush.linearGradient(
-                    colors = listOf(
-                        Color(0xFF47BFDF),
-                        Color(0xFF4A91FF)
-                    ),
-                    start = Offset(Float.POSITIVE_INFINITY, 0f),
-                    end = Offset(0f, Float.POSITIVE_INFINITY)
-                )
-            )
+            .background(MainGradient)
             .padding(top = 60.dp, end = 25.dp, bottom = 20.dp, start = 25.dp)
             .verticalScroll(rememberScrollState())
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text="< Назад", style = textStyle,
-                 modifier = Modifier.clickable { onBack() },
-                 fontSize = 24.sp, fontWeight = FontWeight.Bold, )
+            Text(
+                text = "< Назад", style = textStyle,
+                modifier = Modifier.clickable { onBack() },
+                fontSize = 24.sp, fontWeight = FontWeight.Bold,
+            )
         }
 
         Spacer(modifier = Modifier.height(40.dp))
@@ -79,7 +70,8 @@ fun ShowScreen(weathersToday: List<Weather>,
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.Bottom)
+            verticalAlignment = Alignment.Bottom
+        )
         {
             val result = LocalDateTime.now()
             var date = result.format(formatterFuture)
@@ -93,7 +85,25 @@ fun ShowScreen(weathersToday: List<Weather>,
         WeatherRow(weathersToday)
         Spacer(modifier = Modifier.height(50.dp))
 
-        Text("Прогноз погоды", style = textStyle, fontSize = 22.sp, fontWeight = FontWeight.SemiBold)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        )
+        {
+            Text(
+                "Прогноз погоды",
+                style = textStyle,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Icon(
+                painter = painterResource(R.drawable.calendar),
+                tint = Color.White,
+                contentDescription = "иконка)",
+                modifier = Modifier.size(26.dp)
+            )
+        }
         Spacer(modifier = Modifier.height(25.dp))
         WeatherForecast(weathersForecast)
     }
@@ -101,8 +111,10 @@ fun ShowScreen(weathersToday: List<Weather>,
 
 @Composable
 fun WeatherRow(weathers: List<Weather>) {
-    Row (modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
 
         val currentTime = LocalDateTime.now().hour
 
@@ -114,9 +126,11 @@ fun WeatherRow(weathers: List<Weather>) {
 
 @Composable
 fun WeatherForecast(weathers: List<Weather>) {
-    Column(modifier = Modifier.fillMaxHeight(),
-            verticalArrangement = Arrangement.SpaceAround,
-            horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
         val result = LocalDate.now()
 
@@ -131,45 +145,71 @@ fun WeatherForecast(weathers: List<Weather>) {
 
 @Composable
 fun ForecastRow(weather: Weather, curDate: String) {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(horizontal = 4.dp, vertical = 5.dp),
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 6.dp, vertical = 5.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = curDate, style = textStyle, fontSize = 18.sp,
-            fontWeight = FontWeight.SemiBold, modifier = Modifier.weight(1f))
+        Text(
+            text = curDate, style = textStyle, fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold, modifier = Modifier.weight(1f)
+        )
 
-        Icon(painter = getPainterResource(weather.icon),
+        Icon(
+            painter = getPainterResource(weather.icon),
             contentDescription = "иконка",
-            modifier = Modifier.size(45.dp), tint = Color.White)
+            modifier = Modifier.size(45.dp), tint = Color.White
+        )
 
-        Text(text = "${round(weather.degrees).toInt()}°C", style = textStyle, fontSize = 18.sp, fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.weight(1f), textAlign = TextAlign.End)
+        Text(
+            text = "${round(weather.degrees).toInt()}°C",
+            style = textStyle,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.End
+        )
     }
 }
 
 @Composable
 fun WeatherColumn(weather: Weather, currHour: Int) {
-    Column(modifier = Modifier.height(150.dp),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            Alignment.CenterHorizontally)
+    Column(
+        modifier = Modifier.height(150.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        Alignment.CenterHorizontally
+    )
     {
 
-        Text(text = "${round(weather.degrees)}°C", style = textStyle, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
-        Icon(painter = getPainterResource(weather.icon),
+        Text(
+            text = "${round(weather.degrees)}°C",
+            style = textStyle,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Icon(
+            painter = getPainterResource(weather.icon),
             contentDescription = "иконка",
             modifier = Modifier.size(64.dp),
             tint = Color.White,
         )
-        Text(text = "$currHour:00", style = textStyle, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
+        Text(
+            text = "$currHour:00",
+            style = textStyle,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold
+        )
     }
 }
 
 @Composable
-fun DescriptionScreen(weathersToday: List<Weather>,
-                      weathersForecast: List<Weather>,
-                      onBack: () -> Unit) {
+fun DescriptionScreen(
+    weathersToday: List<Weather>,
+    weathersForecast: List<Weather>,
+    onBack: () -> Unit
+) {
     val density = LocalDensity.current
     val visibleState = remember { MutableTransitionState(false) }
 
@@ -191,13 +231,13 @@ fun DescriptionScreen(weathersToday: List<Weather>,
 @Composable
 fun ScreenPreview() {
     val weathers = listOf(
-        Weather(degrees = 52.5, weatherType = "порно", windKph = 11.1),
-        Weather(degrees = 52.5, weatherType = "порно", windKph = 11.1),
-        Weather(degrees = 52.5, weatherType = "порно", windKph = 11.1),
-        Weather(degrees = 52.5, weatherType = "порно", windKph = 11.1),
-        Weather(degrees = 52.5, weatherType = "порно", windKph = 11.1),
-        Weather(degrees = 52.5, weatherType = "порно", windKph = 11.1),
-        Weather(degrees = 52.5, weatherType = "порно", windKph = 11.1)
+        Weather(degrees = 52.5, weatherType = "хз", windKph = 11.1),
+        Weather(degrees = 52.5, weatherType = "хз", windKph = 11.1),
+        Weather(degrees = 52.5, weatherType = "хз", windKph = 11.1),
+        Weather(degrees = 52.5, weatherType = "хз", windKph = 11.1),
+        Weather(degrees = 52.5, weatherType = "хз", windKph = 11.1),
+        Weather(degrees = 52.5, weatherType = "хз", windKph = 11.1),
+        Weather(degrees = 52.5, weatherType = "хз", windKph = 11.1),
     )
     DescriptionScreen(
         weathersToday = weathers,
